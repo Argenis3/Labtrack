b3412a7e3ee93c61cd9b3ebe4d445ab22beea814
ArgenisAragon <argenisss70@gmail.com>
2025-11-30 22:26:03 -0600

    first commmit

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1wk6360Hd1aGE6G5FX9PK6DTJRBt6qhs",
+  authDomain: "labtrack-diseno.firebaseapp.com",
+  projectId: "labtrack-diseno",
+  storageBucket: "labtrack-diseno.firebasestorage.app",
+  messagingSenderId: "113025864522",
+  appId: "1:113025864522:web:24dbbdce077af268f5e7b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
